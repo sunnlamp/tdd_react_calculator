5bbffbc44875b9b291cf29fafb264c3c9210939b
Rogelio Zamudio <yeyozaz@gmail.com>
2018-12-12 18:07:21 -0600

    completed Calculator edits, updated snapshot, all tests pass

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -7,7 +7,7 @@ class Calculator extends Component {
   state = {
     displayValue: '0',
     numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0', 'ce'],
-    operators: [],
+    operators: ['/', 'x', '-', '+'],
     selectedOperator: '',
     storesValue: ''
   }
